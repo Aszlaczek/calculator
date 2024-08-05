650565071ec00367ce3c98802419b73a0b61841d
adrianwzorek <92018288+adrianwzorek@users.noreply.github.com>
2024-08-05 15:40:37 +0200

    Make new calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,49 @@
-import { useState } from 'react'
-import Buttons from './components/Buttons'
-import './css/App.css'
+import { useState } from "react";
+import "./css/App.css";
+import Buttons from "./components/Buttons";
 
-function App() {
+const App = () => {
+  const listAll = [
+    "X",
+    "/",
+    "AC",
+    "Delete",
+    7,
+    8,
+    9,
+    "^",
+    4,
+    5,
+    6,
+    "-",
+    1,
+    2,
+    3,
+    "+",
+    0,
+    ".",
+    "=",
+  ];
 
-  const [number, setNumber] = useState('');
-  const [answer, setAnswer] = useState('');
-  const [operation, setOperation] = useState('');
-  const buttons: string[] = [
-    'C', '%', 'del', '/',
-    '1', '2', '3', 'X',
-    '4', '5', '6', '-',
-    '7', '8', '9', '+',
-    '0', '.', '='
-  ]
-  const numbers: string[] = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
-  ]
-
-  function decision(e: string) {
-    numbers.includes(e)
-      ? setNumber(prev => prev + e)
-      : operaction(e)
-  }
-
-  function calculate() {
-    if (operation === '' || number === '') {
-      alert('Podaj liczbę')
-      return setNumber(answer)
-    }
-    let result: number = parseFloat(answer);
-    switch (operation) {
-      case '+':
-        result += parseFloat(number)
-        break
-      case '-':
-        result -= parseFloat(number)
-        break
-      case 'x':
-        result *= parseFloat(number)
-        break
-      case '/':
-        number === '0' ? alert('nie można dzielić przez 0')
-          : result /= parseFloat(number)
-        break
-      case '%':
-        result %= parseFloat(number)
-        break
-    }
-    setNumber(result.toString())
-  }
-
-  function operaction(e: string) {
-    switch (e) {
-      case '.':
-        number.includes('.') ? '' :
-          number === '' ? setNumber('0.') : setNumber(prev => prev + '.')
-        break
-      case 'C':
-        setNumber('')
-        setAnswer('')
-        setOperation('')
-        break
-      case 'del':
-        setNumber(prev => prev.slice(0, -1))
-        break
-      case '+':
-        if (number !== '') {
-          setAnswer(number)
-          setNumber('')
-          setOperation('+')
-        }
-        break;
-      case '-':
-        if (number !== '') {
-          setAnswer(number)
-          setNumber('')
-          setOperation('-')
-        }
-        break
-      case 'X':
-        if (number !== '') {
-          setAnswer(number)
-          setNumber('')
-          setOperation('x')
-        }
-        break
-      case '/':
-        if (number !== '') {
-          setAnswer(number)
-          setNumber('')
-          setOperation('/')
-        }
-        break
-      case '%':
-        if (number !== '') {
-          alert('Będziesz wykonywać działanie modulo (reszta z dzielenia)')
-          setAnswer(number)
-          setNumber('')
-          setOperation('%')
-        }
-        break
-      case '=':
-        // Maprawić NaN przy naciśnięciu = bez liczb
-        calculate()
-        setAnswer('')
-        setOperation('')
-        setOperation('')
-        break
-    }
-  }
-  function show() {
-    if (operation !== '') {
-      return (answer + operation + number)
-    } else {
-      return ''
-    }
-  }
+  const [number, setNumber] = useState("0");
+  const [res, setRes] = useState("");
 
   return (
-    <div className="main-container">
-      <div className="card h-100 ">
-        <div className="card-body">
-          <h1 className='text-center'>Hello</h1>
-          <span className='result'>
-            <div className="answer">{show()}</div>
-            <div className="answer">{number}</div>
-          </span>
-          <div className="container buttons">
-            <Buttons buttons={buttons} handleClick={(el: string) => decision(el)} />
-          </div>
-        </div>
+    <main>
+      <div id="title">
+        <span className="res1">{res}</span>
+        <span className="res2">{number}</span>
       </div>
-    </div>
-  )
-}
+      <div className="buttons">
+        <Buttons
+          list={listAll}
+          number={number}
+          setNumber={setNumber}
+          res={setRes}
+        />
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,19 +1,63 @@
-const Buttons = (props: { buttons: string[], handleClick: Function }) => {
-    // Przypisanie odpowiedniej wartości do zmiennej 
-    // ze zmiennej typu props
-    const { buttons, handleClick } = props
-
-    return (
-        <>
-            {buttons.map((el, i) => {
-                return (
-                    <button type='button' id={'btn' + i} onClick={() => handleClick(el)} className="item" key={i} > {el}</button >
-                )
-            })}
-        </>
-    )
-}
-
-export default Buttons
+import { useState } from "react";
 
 
+
+const Buttons = (props: {
+  list: (string | number)[];
+  number: string;
+  setNumber: Function;
+  res: Function;
+}) => {
+  const [operation, setOperation] = useState("");
+
+  const operations: Operation ={
+    '+': (n1,n2) => n1+n2,
+  }
+  
+  const pushNumber = (e: number) => {
+    let res = props.number;
+    res === "0" ? (res = e.toString()) : (res += e);
+    props.setNumber(res);
+  };
+
+  const calculate = (e: string) => {
+    let res = props.number;
+    let res2 = props.number;
+    console.log(res);
+    switch (e) {
+      case "Delete":
+        res.length === 1 ? (res = "0") : (res = res.slice(0, -1));
+        // res = res.slice(0, -1);
+        break;
+      case "AC":
+        res = "0";
+        break;
+      case "X":
+        props.res(res2 + "x");
+        setOperation("*");
+        res = "0";
+        break;
+      case "=":
+        res = (Number(res) {operation}  Number(props.res)).toString();
+        break;
+    }
+    props.setNumber(res);
+  };
+
+  const action = (e: string | number) => {
+    typeof e !== "string" ? pushNumber(e) : calculate(e);
+  };
+  return (
+    <>
+      {props.list.map((e, id) => {
+        return (
+          <button key={id} onClick={() => action(e)} className="item">
+            {e}
+          </button>
+        );
+      })}
+    </>
+  );
+};
+
+export default Buttons;
