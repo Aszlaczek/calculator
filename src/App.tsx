ea6c3fedb2ecb76992e9b54a78b6510c04f247c9
Aszlaczek <awzorek23@wp.pl>
2024-09-07 16:14:09 +0200

    Finished version 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./css/App.css";
 import Buttons from "./components/Buttons";
 
 const App = () => {
   const listAll = [
-    "X",
+    "*",
     "/",
     "AC",
     "Delete",
@@ -25,22 +25,87 @@ const App = () => {
     "=",
   ];
 
-  const [number, setNumber] = useState("0");
-  const [res, setRes] = useState("0");
+  const calc: { [key: string]: Function } = {
+    "+": (n1: number, n2: number) => n1 + n2,
+    "-": (n1: number, n2: number) => n1 - n2,
+    "*": (n1: number, n2: number) => n1 * n2,
+    "/": (n1: number, n2: number) => n1 / n2,
+    "^": (n1: number, n2: number) => Math.pow(n1, n2),
+    ".": (n1: number) => (n1.toString().includes(".") ? "" : n1 / 10),
+  };
+
+  const [input, setInput] = useState<string>("0");
+  const [operation, setOperation] = useState<string | null>(null);
+  const prevValue = useRef<string | null>(null);
+  const [result, setResult] = useState<string>("");
+
+  const handleButtonClick = (e: string | number) => {
+    if (typeof e === "number" || e === ".") {
+      handleNumberInput(e.toString());
+    } else {
+      handleOperationInput(e as string);
+    }
+  };
+
+  const handleNumberInput = (num: string) => {
+    if (num === ".") {
+      if (input.includes(".")) return;
+    }
+    setInput(input === "0" && num !== "." ? num : input + num);
+  };
+
+  const handleOperationInput = (op: string) => {
+    if (input === "0" && result === "") return;
+    switch (op) {
+      case "AC":
+        resetCalculator();
+        break;
+      case "Delete":
+        handleDelete();
+        break;
+      case "=":
+        if (operation && prevValue !== null) {
+          const newResult = calc[operation](
+            parseFloat(prevValue.current!),
+            parseFloat(input)
+          );
+          setResult("");
+          setInput(newResult.toString());
+          prevValue.current = "0";
+          setOperation(null);
+        }
+        break;
+      case ".":
+        handleNumberInput(op);
+        break;
+      default:
+        setOperation(op);
+        prevValue.current = input;
+        setInput("0");
+        setResult((prevValue.current += op));
+        break;
+    }
+  };
+
+  const handleDelete = () => {
+    setInput(input.length > 1 ? input.slice(0, -1) : "0");
+  };
+
+  const resetCalculator = () => {
+    setInput("0");
+    setOperation(null);
+    prevValue.current = null;
+    setResult("");
+  };
 
   return (
     <main>
       <div id="title">
-        <span className="res1">{res}</span>
-        <span className="res2">{number}</span>
+        <span className="res1">{result}</span>
+        <span className="res2">{input}</span>
       </div>
       <div className="buttons">
-        <Buttons
-          list={listAll}
-          number={number}
-          setNumber={setNumber}
-          res={setRes}
-        />
+        <Buttons list={listAll} onClick={handleButtonClick} />
       </div>
     </main>
   );
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,72 +1,9 @@
-import { useState } from "react";
-
-const calc: { [key: string]: Function } = {
-  "+": (n1: string, n2: string) => (Number(n1) + Number(n2)).toString(),
-  "-": (n1: string, n2: string) => (Number(n1) - Number(n2)).toString(),
-  "*": (n1: string, n2: string) => (Number(n1) * Number(n2)).toString(),
-  "/": (n1: string, n2: string) => (Number(n1) / Number(n2)).toString(),
-  "^": (n1: string, n2: string) => Math.pow(Number(n1), Number(n2)).toString(),
-  ".": (n1: string) => (Number(n1) * -1).toString(),
-};
-const Buttons = (props: {
-  list: (string | number)[];
-  number: string;
-  setNumber: Function;
-  res: Function;
-}) => {
-  const pushNumber = (e: number) => {
-    let res = props.number;
-    res === "0" ? (res = e.toString()) : (res += e);
-    props.setNumber(res);
-  };
-
-  const operation = {
-    add: "+",
-    sub: "-",
-    mul: "*",
-    div: "/",
-    pow: "^",
-    dot: ".",
-  };
-  const [op, setOp] = useState("");
-  const [val, setVal] = useState("");
-  const calculate = (e: string) => {
-    let res = props.number;
-    console.log(res);
-    let val = res;
-    switch (e) {
-      case "Delete":
-        res.length === 1 ? (res = "0") : (res = res.slice(0, -1));
-        break;
-      case "AC":
-        res = "0";
-        props.res("0");
-        break;
-      case "X":
-        props.res(res + "x");
-        setVal(res);
-        // res = calc[operation.mul](val, res);
-        setOp(operation.mul);
-        res = "0";
-        break;
-      case "=":
-        props.res("0");
-        val = "";
-        break;
-    }
-    res = calc[op](res, val);
-    props.setNumber(res);
-    setOp("");
-  };
-
-  const action = (e: string | number) => {
-    typeof e !== "string" ? pushNumber(e) : calculate(e);
-  };
+const Buttons = (props: { list: (string | number)[]; onClick: Function }) => {
   return (
     <>
       {props.list.map((e, id) => {
         return (
-          <button key={id} onClick={() => action(e)} className="item">
+          <button key={id} onClick={() => props.onClick(e)} className="item">
             {e}
           </button>
         );
